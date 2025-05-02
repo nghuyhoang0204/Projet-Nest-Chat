f55b2346e50673a10e3fe72b9d2a2e476b335b52
nghuyhoang <Aries.THs@gmail.com>
2025-05-02 21:27:12 +0200

    Setup backend

---
+++ src/server/app.module.ts
@@ -0,0 +1,21 @@
+import { Module } from '@nestjs/common';
+import { ChatModule } from '../server/chat/chat.module';
+import { ServeStaticModule } from '@nestjs/serve-static';
+import { join } from 'path';
+import { RoomModule } from '../server/room.module';
+import { CaslModule } from '../server/casl.module';
+import { UserModule } from '../server/user/user.module';
+
+@Module({
+    imports: [
+        ChatModule,
+        ServeStaticModule.forRoot({
+            rootPath: join(__dirname, '..', '..', '..', 'dist', 'client'),
+            exclude: ['/api*'],
+        }),
+        RoomModule,
+        CaslModule,
+        UserModule,
+    ],
+})
+export class AppModule { }
